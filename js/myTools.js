2f04164ad406ab014d1906f65662638a812d1290
Jay Zangwill <359390851@qq.com>
2018-02-21 22:07:41 +0800

    push

--- js/myTools.js
+++ js/myTools.js
@@ -2,12 +2,25 @@
     function Jay() {};
     Jay.prototype = {
         constructor: Jay,
+        //获取dom
         $: function (selector) {
             if (selector.substr(0, 1) === '#') {
                 return document.getElementById(selector.slice(1));
             } else {
                 var node = document.querySelectorAll(selector);
                 return node.length > 1 ? node : node[0];
+            }
+        },
+        //函数防抖
+        debounce: function (fn, delay, now) {
+            var timer;
+            return function () {
+				if(now) {
+					now = false;
+					fn();
+				}
+                clearTimeout(timer);
+                timer = setTimeout(fn, delay);
             }
         }
     }
