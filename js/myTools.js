1ba5b03265f3fee79b4d62f1bcd812c833493252
Jay Zangwill <359390851@qq.com>
2018-02-22 22:20:36 +0800

    pix bug and add function

--- js/myTools.js
+++ js/myTools.js
@@ -15,13 +15,45 @@
         debounce: function (fn, delay, now) {
             var timer;
             return function () {
-				if(now) {
-					now = false;
-					fn();
-				}
+                var self = this;
+                if (now) {
+                    now = false;
+                    fn.call(self);
+                }
                 clearTimeout(timer);
-                timer = setTimeout(fn, delay);
+                timer = setTimeout(function () {
+                    fn.call(self);
+                }, delay);
             }
+        },
+        //函数节流
+        throttle: function (fn, delay, time) {
+            var timer,
+                oldTime = new Date(),
+                now;
+            return function () {
+                var self = this;
+                if ((now = new Date()) - oldTime > time) {
+                    fn.call(self);
+                    oldTime = now;
+                }
+                clearTimeout(timer);
+                timer = setTimeout(function () {
+                    fn.call(self);
+                }, delay);
+            }
+        },
+        //事件绑定
+        on: function (node, type, fn, option) {
+            option == undefined && (option = false);
+            window.addEventListener ? node.addEventListener(type, fn, option) : node.attachEvent('on' +
+                type, fn);
+        },
+        //事件取消
+        off: function (node, type, fn, option) {
+            option == undefined && (option = false);
+            window.removeEventListener ? node.removeEventListener(type, fn, option) : node.detachEvent('on' +
+                type, fn);
         }
     }
     window.jay = new Jay();
