de24d5fa20745440c76c9eac167372da84cadbc2
Jay Zangwill <359390851@qq.com>
2018-02-24 18:19:12 +0800

    add fun

--- js/myTools.js
+++ js/myTools.js
@@ -48,7 +48,83 @@
         //判断类型
         nativeType: function(obj) {
             var type = typeof obj;
-            return type === 'object' ? {}.toString.call(obj) : type;
+            if (type === 'object') {
+                var objType = {}.toString.call(obj);
+                return /\b(\w+)]/.exec(objType)[1].toLowerCase();
+            }
+            return type;
+        },
+        //获取url参数
+        getParameter: function(url, key) {
+            var paramate = {};
+            if (url === true) {
+                url = window.location.search;
+            } else if (!/\?/.test(url)) {
+                return;
+            }
+            if (this.nativeType(key) === 'array') {
+                for (var i = key.length; i--;) {
+                    new RegExp('(?:[?|&])' + key[i] + '=([^&]*)', 'ig').test(url) && (paramate[key[i]] = RegExp.$1);
+                }
+                return paramate;
+            } else if (this.nativeType(key) === 'string') {
+                new RegExp('(?:[?|&])' + key + '=([^&]*)', 'ig').test(url) && (paramate[key] = RegExp.$1);
+                return paramate;
+            } else {
+                var reg = /(?:[?|&])(.+?)=([^&]*)/ig;
+                while (reg.test(url)) {
+                    paramate[RegExp.$1] = RegExp.$2;
+                }
+                return paramate;
+            }
+
+        },
+        //ajax
+        ajax: function(option) {
+            if (option == undefined || this.nativeType(option.url) !== 'string') {
+                return;
+            }
+            this.nativeType(option.succss) !== 'function' && console.warn('missing succss function or success is no a function');
+            var xhr = new XMLHttpRequest(),
+                url = option.url,
+                type = option.type || 'get',
+                dataType = option.dataType || 'json',
+                contentType = option.contentType || 'application/x-www-form-urlencoded; charset=UTF-8',
+                data,
+                temp = [];
+            type = type.toLowerCase();
+            dataType = dataType.toLowerCase();
+            if (option.data && this.nativeType(option.data) === 'string') {
+                data = option.data;
+            } else if (option.data && this.nativeType(option.data) === 'object') {
+                for (var key in option.data) {
+                    temp.push(key + '=' + option.data[key]);
+                }
+                data = temp.join('&');
+            }
+            (type === 'get' || type === 'jsonp') && data && (url += '?' + data);
+            if (type === 'jsonp') {
+                var callbackName = option.callbackName || 'callback',
+                    script = document.createElement('script'),
+                    random = ('' + Math.random() + Math.random()).replace(/0\./g, '_');
+                script.src = url + '&' + callbackName + '=jayTools' + random;
+                document.body.appendChild(script);
+                window['jayTools' + random] = option.succss;
+                document.body.removeChild(script);
+                return;
+            }
+            xhr.responseType = dataType;
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4) {
+                    if (xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
+                        option.succss.call(this, dataType === 'json' ? xhr.response : dataType === 'xml' ? xhr.responseXML : xhr.responseText);
+                    } else {
+                        option.error && option.error.call(this, xhr.status, xhr.statusText);
+                    }
+                }
+            }
+            xhr.open(type, url, true);
+            type === 'get' ? xhr.send(null) : xhr.send(data), xhr.contentType = contentType;
         }
     }
     window.jay = new Jay();
