a197ec17de389e75402a48ff7f2ea75ebf86355d
Jay Zangwill <359390851@qq.com>
2018-03-30 15:53:26 +0800

    update

---
+++ gulp/README.md
@@ -0,0 +1,9 @@
+ # 使用方法
+ 
+```bash
+    npm i //安装依赖
+    gulp dev //开发模式（默认监听3000）
+    gulp build //生产模式
+    gulp dev -p //随机指定端口
+    gulp dev -p 8080 //指定8080为开发端口
+```
--- js/myTools.js
+++ js/myTools.js
@@ -1,11 +1,11 @@
 'use strict';
 
-(function (window, document) {
+(function(window, document, undefined) {
     function Jay(selector) {};
     Jay.prototype = {
         constructor: Jay,
         //获取元素
-        $: function (selector) {
+        $: function(selector) {
             if (selector[0] === '#') {
                 return document.getElementById(selector.slice(1));
             } else {
@@ -14,39 +14,39 @@
             }
         },
         //函数防抖
-        debounce: function (fn, delay, now) {
+        debounce: function(fn, delay, now) {
             var timer;
-            return function () {
+            return function() {
                 var self = this;
                 if (now) {
                     now = false;
                     fn.call(self);
                 }
                 clearTimeout(timer);
-                timer = setTimeout(function () {
+                timer = setTimeout(function() {
                     fn.call(self);
                 }, delay);
             }
         },
         //函数节流
-        throttle: function (fn, delay, time) {
+        throttle: function(fn, delay, time) {
             var timer,
                 oldTime = new Date(),
                 now;
-            return function () {
+            return function() {
                 var self = this;
                 if ((now = new Date()) - oldTime > time) {
                     fn.call(self);
                     oldTime = now;
                 }
                 clearTimeout(timer);
-                timer = setTimeout(function () {
+                timer = setTimeout(function() {
                     fn.call(self);
                 }, delay);
             }
         },
         //判断类型
-        nativeType: function (obj) {
+        nativeType: function(obj) {
             var type = typeof obj;
             if (type === 'object') {
                 var objType = {}.toString.call(obj);
@@ -55,7 +55,7 @@
             return type;
         },
         //获取url参数
-        getParameter: function (url, key) {
+        getParameter: function(url, key) {
             var paramate = {};
             if (url === true) {
                 url = window.location.search;
@@ -81,7 +81,7 @@
 
         },
         //ajax
-        ajax: function (option) {
+        ajax: function(option) {
             if (option == undefined || this.nativeType(option.url) !== 'string') {
                 return;
             }
@@ -99,9 +99,9 @@
             if (option.data && this.nativeType(option.data) === 'string') {
                 data = option.data;
             } else if (option.data && this.nativeType(option.data) === 'object') {
-                for (var key in option.data) {
+                Object.keys(option.data).forEach(function(key) {
                     temp.push(key + '=' + option.data[key]);
-                }
+                })
                 data = temp.join('&');
             }
             (type === 'get' || type === 'jsonp') && data && (url += '?' + data);
@@ -116,7 +116,7 @@
                 return;
             }
             xhr.responseType = dataType;
-            xhr.onreadystatechange = function () {
+            xhr.onreadystatechange = function() {
                 if (xhr.readyState === 4) {
                     if (xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
                         option.success.call(this, dataType === 'json' ? xhr.response : dataType ===
@@ -130,38 +130,44 @@
             type === 'get' ? xhr.send(null) : xhr.send(data), xhr.contentType = contentType;
         },
         //对象合并
-        objMerge: function (target, obj) {
-            for (var key in obj) {
-                target[key] && this.nativeType(target[key]) === 'object' ? this.objMerge(target[key], obj[
-                    key]) : target[key] = obj[key];
-            }
+        objMerge: function(target, obj) {
+            Object.keys(obj).forEach(function(key) {
+                target[key] && this.nativeType(target[key]) === 'object' ? this.objMerge(target[key], obj[key]) : target[key] = obj[key];
+            })
             return target;
         },
         //判断对象是否相等
-        objEqual: function (target, obj) {
+        objEqual: function(target, obj) {
             if (target === obj) {
                 return true;
             }
             return JSON.stringify(target) === JSON.stringify(obj);
         },
         //对象拷贝
-        clone: function (obj, deep) {
-            var result = {};
-            if (this.nativeType(obj) !== 'object') {
-                return;
+        clone: function(obj, deep) {
+            if (obj === null) {
+                return null;
             }
+            if (typeof obj !== 'object') {
+                return obj;
+            }
+            var result = this.nativeType(obj) === 'array' ? [] : {},
+                self = this,
+                keys = Object.keys(obj);
             if (deep) {
-                result = JSON.parse(JSON.stringify(obj));
+                keys.forEach(function(key) {
+                    result[key] = self.nativeType(obj[key]) === 'object' || self.nativeType(obj[key]) === 'array' ? self.clone(obj[key]) : obj[key];
+                })
             } else {
-                for (var key in obj) {
+                keys.forEach(function(key) {
                     result[key] = obj[key];
-                }
+                })
             }
             return result;
         },
         //在节点后插入
-        insertAfter: function (target, node) {
-            if (target.nodeType!==1 || node.nodeType !== 1) {
+        insertAfter: function(target, node) {
+            if (target.nodeType !== 1 || node.nodeType !== 1) {
                 throw TypeError(target + 'or' + node + 'is no a html node!');
             }
             var next = target.nextSibling,
